6e4197167f0c80feaf5fc0caa428ee58b0e54725
Davidson Ratis <davidsonmakarios@gmail.com>
2020-08-15 14:39:27 -0300

    configuring the plugins

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store/store.js'
+import './plugins/axios.js'
+import './plugins/bootstrap-vue.js'
 
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')

2e0655f9be7e07fc4b7a60237384232eb0c51f48
Davidson Ratis <davidsonmakarios@gmail.com>
2020-08-15 14:46:38 -0300

    initial components

---
+++ src/views/Crud.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="crud">
+        <h2>Crud com VueJS</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+.crud h2 {
+    text-align: center;
+    margin-bottom: 30px;
+}
+</style>

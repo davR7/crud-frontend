3e7612843d25ca3fa4e052603ee9d3ae70782d0a
Davidson Ratis <davidsonmakarios@gmail.com>
2020-08-19 16:40:37 -0300

    updated routes

--- src/router.js
+++ src/router.js
@@ -1,13 +1,24 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import AddUser from './components/AddUser.vue'
+import UserTable from './views/UserTable.vue'
+import AddUser from './views/AddUser.vue'
+import EditUser from './views/EditUser.vue'
 
 Vue.use(Router)
 
 export default new Router({
     mode: 'history',
     routes: [{
+        path: '/',
+        component: UserTable
+    }, {
         path: '/adduser',
         component: AddUser
+    }, {
+        path: '/edituser',
+        component: EditUser
+    }, {
+        path: '*',
+        redirect: '/'
     }]
 })
